c7bc6abf9ecdf7f12b915cfb0b386122976b4232
sciencetalk304-art <sciencetalk304@gmail.com>
2026-06-29 23:52:54 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+controller.player4.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Released, function () {
+	
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+	
+})
+controller.player2.onEvent(ControllerEvent.Connected, function () {
+	
+})
